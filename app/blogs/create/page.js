8ff1f37b682eaec9f27f299017b951dd5730dc9f
mangala namal <mangalanamal@gmail.com>
2025-06-25 22:48:55 +0530

    fix5

--- app/blogs/create/page.js
+++ app/blogs/create/page.js
@@ -1,7 +1,7 @@
 'use client';
 export const dynamic = 'force-dynamic';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import {
   ArrowLeftIcon,
